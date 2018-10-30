187f1549e2495241e641194f41a642b16a1014b8
Robrens <jbb.baudu@wanadoo.fr>
2018-10-30 14:50:46 +0100

    override with price comparison

--- woocommerce/single-product/price.php
+++ woocommerce/single-product/price.php
@@ -5,5 +5,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $product;
 
+if ($product->get_price() < 20) {
 ?>
+<p>Golden Offer !!!!</p>
 <p class="price">Price : <?php echo $product->get_price_html(); ?></p>
+<?php
+}
+else {
+?>
+<p>No special offer</p>
+<p class="price">Price : <?php echo $product->get_price_html(); ?></p>
+<?php
+}
